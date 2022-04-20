9255416dfeb6ab82721e4f2dcd04ddc37c661d13
Huseyin BABAL <huseyinbabal88@gmail.com>
2022-04-20 16:02:08 +0300

    port fixed

--- main.go
+++ main.go
@@ -4,11 +4,12 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
 		fmt.Fprint(writer, "Hello world.")
 	})
-	log.Fatal(http.ListenAndServe(":3000", nil))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", os.Getenv("PORT")), nil))
 }
